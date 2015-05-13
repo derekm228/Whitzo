62883a8e02f190738fa3ac025c71f3b0755d69e5
Derek Miller <derekm228@gmail.com>
2015-05-13 13:22:58 -0400

    awd

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ $(document).ready(function(){
   		// Create the autocomplete object, restricting the search
   		// to geographical location types.
   		autocomplete = new google.maps.places.Autocomplete(
-     		(document.getElementById('autocomplete')),
+     		(document.getElementById('address')),
       		{ types: ['geocode'] });
   			// When the user selects an address from the dropdown,
   			// populate the address fields in the form.
