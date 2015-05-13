d21aa996c912a1c1a1e431e82e82765564f8b776
Derek Miller <derekm228@gmail.com>
2015-05-13 13:16:05 -0400

    Geolocation for text input

--- js/main.js
+++ js/main.js
@@ -57,22 +57,61 @@ $(document).ready(function(){
 	//Event listeners
 	google.maps.event.addDomListener(window, 'load', initialize);
 
+/*-----------------------------------------ADDRESS SEARCHING SPECIFIC CODE-----------------------------------------*/
+
+	// This example displays an address form, using the autocomplete feature
+	// of the Google Places API to help users fill in the information.
+
+	var placeSearch, autocomplete;
+
+	function initializeAuto() {
+  		// Create the autocomplete object, restricting the search
+  		// to geographical location types.
+  		autocomplete = new google.maps.places.Autocomplete(
+     		// @type {HTMLInputElement} (document.getElementById('autocomplete')),
+      		{ types: ['geocode'] });
+  			// When the user selects an address from the dropdown,
+  			// populate the address fields in the form.
+  		google.maps.event.addListener(autocomplete, 'place_changed');
+	}
+
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+	 	if (navigator.geolocation) {
+	    	navigator.geolocation.getCurrentPosition(function(position) {
+	      		var geolocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	      		var circle = new google.maps.Circle({
+	        		center: geolocation,
+	        		radius: position.coords.accuracy
+	      		});
+	      		autocomplete.setBounds(circle.getBounds());
+	    	});
+	 	}
+	}
+
+	initializeAuto();
+
+	$("#address").on('focus', geolocate);
+
 /*------------------------------------------CALENDAR SPECIFIC START CODE------------------------------------------*/
 
 	datePickerController.createDatePicker({
-    formElements: {
-        "moveInDate": "%d/%m/%Y"
-    },
-    // Position the activation button within the span
-    nopopup: true
-});
+    	formElements: {
+        	"moveInDate": "%m/%d/%Y"
+    	},
+    	// Position the activation button within the span
+    	nopopup: true
+	});
+	
 	datePickerController.createDatePicker({
-    formElements: {
-        "moveOutDate": "%d/%m/%Y"
-    },
-    // Position the activation button within the span
-    nopopup: true
-});
+    	formElements: {
+        	"moveOutDate": "%m/%d/%Y"
+    	},
+    	// Position the activation button within the span
+    	nopopup: true
+	});
+
 
 /*------------------------------------------ANIMATE SCROLLING WHEN SELECTING LINKS------------------------------------------*/
 
