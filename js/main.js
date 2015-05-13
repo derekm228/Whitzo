edf1b7bc0342f42ae1e4013252e11b1a7db53c5a
Derek Miller <derekm228@gmail.com>
2015-05-13 09:22:30 -0400

    Changes

--- js/main.js
+++ js/main.js
@@ -95,32 +95,29 @@ $(document).ready(function(){
 	});
 
 /*------------------------------------------GOOGLE PLUS SIGN IN CODE------------------------------------------*/
-	
-	var additionalParams = {
-	    'theme' : 'dark',
-	    'callback': signinCallback
+	/*
+	function render() {
+
+	  // Additional params
+	  var additionalParams = {
+	    'theme' : 'dark'
 	  };
 
-	function signinCallback(authResult) {
-  		if (authResult['status']['signed_in']) {
-    		// Update the app to reflect a signed in user
-    		// Hide the sign-in button now that the user is authorized, for example:
-    		$('#loginButton').attr('style', 'display: none');
-  		} 
-  		else {
-    		// Update the app to reflect a signed out user
-    		// Possible error values:
-    		//   "user_signed_out" - User is signed-out
-    		//   "access_denied" - User denied access to your app
-    		//   "immediate_failed" - Could not automatically log in the user
-    		console.log('Sign-in state: ' + authResult['error']);
-  		}
+	  gapi.signin.render('myButton', additionalParams);
 	}
+
+
+
+	var additionalParams = {
+	    'theme' : 'dark'
+	  };
+
+	gapi.signin.render('loginButton', additionalParams);
 
 	// Attach a click listener to a button to trigger the flow.
 	$('#loginButton').on('click', function() {
 		gapi.auth.signIn(additionalParams); // Will use page level configuration
-   	});
+   	});*/
 });
 
 
